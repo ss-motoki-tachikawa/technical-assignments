ebba4b6653702812599b0b1e9a8be1797a5e6e1c
ss-motoki-tachikawa <motoki.tachikawa@tokyo.supersoftware.co.jp>
2024-04-11 15:51:04 +0900

    コンストラクタ試す

--- PHPv-2/peach.php
+++ PHPv-2/peach.php
@@ -1,8 +1,12 @@
 <?php
     require_once("fruit.php");
     class Peach extends Fruit{
-        public $min_price = 200;
-        public $max_price = 300;
+        public $min_price;
+        public $max_price;
+        public function __construct(){
+            $this->min_price = 200;
+            $this->max_price = 300;
+        }
 
         public function callGetPrice(){
             $prices = $this->getPrice($this->min_price, $this->max_price);
--- PHPv-2/strawberry.php
+++ PHPv-2/strawberry.php
@@ -1,9 +1,12 @@
 <?php
     require_once("fruit.php");
     class Strawberry extends Fruit{
-        public $min_price = 400;
-        public $max_price = 500;
-
+        public $min_price;
+        public $max_price;
+        public function __construct(){
+            $this->min_price = 200;
+            $this->max_price = 300;
+        }
         public function callGetPrice(){
             $prices = $this->getPrice($this->min_price, $this->max_price);
             return $prices;
