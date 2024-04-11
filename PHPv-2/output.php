b63196ce3aeb6730f615a23f840056df9d76dfd7
ss-motoki-tachikawa <motoki.tachikawa@tokyo.supersoftware.co.jp>
2024-04-11 15:25:17 +0900

    コード修正

--- PHPv-2/output.php
+++ PHPv-2/output.php
@@ -1,10 +1,20 @@
 <?php
     require_once("result.php");
-    if(isset($_POST["peach"])){
-        $fruit = "もも";
-    }elseif(isset($_POST["strawberry"])){
-        $fruit = "いちご";
+    class Output {
+        public function output() {
+            if(isset($_POST["peach"])){
+                $fruit = "もも";
+            }elseif(isset($_POST["strawberry"])){
+                $fruit = "いちご";
+            }
+            $result = new Result();
+            $results = $result->callGetResults();
+            echo "果物:" . $fruit . "<br/>";
+            echo "最大値:" . $results[0] . "円" . "<br/>";
+            echo "最低値:" . $results[1] . "円" . "<br/>";
+            echo "平均値:" .round($results[2]) . "円" . "<br/>";
+        }
     }
-    $output = new Result();
-    $output->output($fruit);
+    $output = new Output();
+    $output->output();
 ?>
--- PHPv-2/result.php
+++ PHPv-2/result.php
@@ -16,19 +16,12 @@
             $calculation = new Calculation();
             return $calculation;
         }
-        public function Results() {
+        public function callGetResults() {
             $fruits = $this->setFruits();
             $prices = $fruits->callGetPrice();
             $calculation = $this->callCalculation();
             $results = $calculation->getResult($prices);
             return $results;
-        }
-        public function output($fruit) {
-            $results = $this->Results();
-            echo "果物:" . $fruit . "<br/>";
-            echo "最大値:" . $results[0] . "円" . "<br/>";
-            echo "最低値:" . $results[1] . "円" . "<br/>";
-            echo "平均値:" .round($results[2]) . "円" . "<br/>";
         }
     }
     
