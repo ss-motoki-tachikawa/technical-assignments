29e4f274498781e81ae5d3a53842d6d602b84a54
ss-motoki-tachikawa <motoki.tachikawa@tokyo.supersoftware.co.jp>
2024-04-11 15:20:13 +0900

    結果処理修正

--- PHPv-2/result.php
+++ PHPv-2/result.php
@@ -1,4 +1,3 @@
-<!-- 結果を返す画面　桃クラスから価格を受け取って、計算クラスに渡す。 -->
 <?php
     require_once("peach.php");
     require_once("strawberry.php");
@@ -7,25 +6,25 @@
         public function setFruits() {
             if(isset($_POST["peach"])){
                 $fruits = new Peach();
-            }elseif(isset($_POST["strawberry"])){
+            }
+            if(isset($_POST["strawberry"])){
                 $fruits = new Strawberry();
             }
             return $fruits;
         }
-        public function callFruits() {
-            $fruits = $this->setFruits();
-            $prices = $fruits->callGetPrice();
-            return $prices;
-        }
         public function callCalculation() {
             $calculation = new Calculation();
-            $prices = $this->callFruits();
+            return $calculation;
+        }
+        public function Results() {
+            $fruits = $this->setFruits();
+            $prices = $fruits->callGetPrice();
+            $calculation = $this->callCalculation();
             $results = $calculation->getResult($prices);
             return $results;
         }
-
         public function output($fruit) {
-            $results = $this->callCalculation();
+            $results = $this->Results();
             echo "果物:" . $fruit . "<br/>";
             echo "最大値:" . $results[0] . "円" . "<br/>";
             echo "最低値:" . $results[1] . "円" . "<br/>";
